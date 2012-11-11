5364c0bef705e7a56083a84752c5b9d1aae5ba70
matt <matt@inspirion14z.(none)>
2012-11-11 12:54:46 -0500

    update

---
+++ listy-text/README.md
@@ -0,0 +1,11 @@
+##Listy-Text
+Listy-Text is a JavaScript plugin to make gathering lists of data with text inputs more user friendly and intuitive. It relies on Jquery. See the [project page](http://mwmnj.github.com/listy-text) for examples and usage. 
+
+##Author
+- Email: matthewwilliammeyer@gmail.com
+- Website: www.mwmeyer.com
+
+Feel free to contact me anytime for assistance or information regarding this plugin :)
+
+##License
+http://mwmeyer.mit-license.org
--- listy-text/listy-text.js
+++ listy-text/listy-text.js
@@ -1,77 +1,101 @@
-var count = 1;
-$(document).ready(function() {
+/*!
+ * jQuery Listy-Text Plugin 
+ * http://mwmnj.github.com/listy-text/
+ * License: http://mwmeyer.mit-license.org
+ */
+(function(){
 
-  $(".text-value").change(function() { 
-    var tbox = $(this);
-    return make_listy(tbox);
-  });
-  
-  $(".text-value").keydown(function(e) {
-    
-    var tbox = $(this);
+  themes = ['facebooky', 'bootstrappy'];
+  var count = 1 ;
 
-    // remove deletion highlight 
-    if (e.keyCode != 8 && $('ul li:nth-last-child(2)').children('.item').hasClass('del-highlight')) {
-      $('ul li:nth-last-child(2)').children('.item').removeClass('del-highlight');
+  function RemoveItem() {
+    var li = $(this).parent().parent();
+    var ul = li.parent();
+    li.remove();
+    if( ul.find("div.item").length < 1 ){
+      var tbox = ul.find("input[type!=hidden]") ;
     }
+    ul.trigger("change")
+  }
 
-    if (e.keyCode == 188) {  // if comma
-
-      return make_listy(tbox);
-
-    }
-    else if (e.keyCode == 8)  {  // if backspace
-      if (tbox.val().length < 1) {  // if its been highligheted, delete the item
-        if ($(this).closest('ul').find('li:nth-last-child(2)').children('.item').hasClass('del-highlight')){
-          var remove = ',' + $(this).closest('ul').find('li:nth-last-child(2)').children('.individual-format').val();
-          var new_csv_data = $(this).next('input[name="csv-format"]').val().replace(new RegExp(remove, 'g'),'');
-          $(this).next('input[name="csv-format"]').val(new_csv_data);
-          $(this).closest('ul').find('li:nth-last-child(2)').remove();
-        }
-        else {  // add highlight
-          $(this).closest('ul').find('li:nth-last-child(2)').children('.item').addClass('del-highlight');
-        }
-      }
-    }
-    
-  });
-  
-  //  clicking anywhere on the pseudo-textbox focuses on text input and adds an outline
-  $('.listy-text').click(function() {
-    $('input.text-value', this).focus();
-    $(this).addClass('listy-outline');
-  });
-
-  $('.listy-text').focusout(function() {
-    if ($(this).hasClass('listy-outline')) {
-      $(this).removeClass('listy-outline');
-    }
-  });
-});
-
-function RemoveItem(id) {
-    var remove = ',' + $('#' + id).find('input[name='+id+']').val();
-    var new_csv_data = $('#' + id).parent().find('input[name="csv-format"]').val().replace(new RegExp(remove, 'g'),'');
-    $('#' + id).parent().find('input[name="csv-format"]').val(new_csv_data);
-    $('#' + id).remove();
-}
-
-function make_listy(tbox){
-      tbox.attr('placeholder', '');
-
+  function make_listy(tbox){
+   
+    var val = $.trim( tbox.val() );
+    if( val ){
       // add item as box
-      tbox.closest('li').before($('<li id="list-item' + count + '"><div class="item">' 
-                                  + tbox.val() + ' <a href="javascript:RemoveItem(\'list-item' 
-                                  + count 
-                                  + '\');">x</a></div><input type="hidden" class="individual-format" name="list-item' 
-                                  + count + '" value="' + tbox.val() + '"></li>'));
+      tbox.closest('li').before($('<li id="list-item-listy' + count + '"><div value="'+tbox.val()+'" class="item">' 
+                                + tbox.val() + ' <a href="javascript:void(0);">x</a></div></li>'));
 
-      // store data in a hidden input
-      var csv_data = tbox.next('input[name="csv-format"]').val() + ','+ tbox.val();
-      tbox.next('input[name="csv-format"]').val(csv_data)
+    }
 
-      tbox.val('');
-      count++;
+    tbox.val('');
+    count++;
+    
+    return false;
+  }
+
+  $.fn.listyVal = function(){
+    var items = $(this).parent().parent().find(".item");
+    var results = [] ;
+    items.each(function() { results.push($(this).attr('value')); });
+    return results;
+  }
+  $.fn.listy = function( ){
+    $(this).each(function( i , me  ){
+      me = $(me);
+      var classList = me.attr('class').replace(/listy-text/g,"").split(/\s+/);  // get all classes on input, ignore listy-text
+      me.wrap('<li class="listy-text-li" />');
+      me.parent().wrap('<ul class="listy-text-ul" />');
+
+      for (var i = 0; i < classList.length; i++) {
+          if (themes.indexOf(classList[i]) > -1) {
+            me.removeClass(classList[i]);
+            me.parent().parent().addClass(classList[i]);
+          }
+      }
+
+      $( me ).change(function(){
+        me.parent().parent().trigger("change");
+        return make_listy( $(this) );
+      });
+      $( me ).keydown(function(e) {
+        var tbox = $(this);
+        // remove deletion highlight 
+        if (e.keyCode != 8 && $('li:nth-last-child(2)' , me.parent().parent() ).children('.item').hasClass('del-highlight')) {
+          $('li:nth-last-child(2)' , me.parent().parent() ).children('.item').removeClass('del-highlight');
+        }
+     
+        if (e.keyCode == 188) {  // if comma
+     
+          return make_listy(tbox);
+     
+        }
+        else if (e.keyCode == 8)  {  // if backspace
+          if (tbox.val().length < 1) {  // if its been highligheted, delete the item
+            var item = $(this).closest('ul').find('li:nth-last-child(2)').children('.item');
+            if ( item.hasClass('del-highlight') ){
+              RemoveItem.call( item.find("a") );
+            }
+            else {  // add highlight
+              item.addClass('del-highlight');
+            }
+          }
+        }
+        
+      });
       
-      return false;
-}
+      //  clicking anywhere on the pseudo-textbox focuses on text input and adds an outline
+      me.parent().parent().click(function() {
+        $('input.listy-text', this).focus();
+        $(this).addClass('listy-outline');
+      });
+
+      me.parent().parent().delegate("a","click",RemoveItem);
+      me.parent().parent().focusout(function() {
+        if ($(this).hasClass('listy-outline')) {
+          $(this).removeClass('listy-outline');
+        }
+      });
+    });
+  }
+}(jQuery));
